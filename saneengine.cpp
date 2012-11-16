d0b1c6566d2049c70fb35dccecd32336f801b85c
Elias Probst <mail@eliasprobst.eu>
2012-11-16 23:31:43 +0100

    Initial basic implementation for the SANE engine which should now be able to list the Scanner data source and return 3 hardcoded scanner names as result.

---
+++ saneengine.cpp
@@ -0,0 +1,58 @@
+/*
+ * A KDE Plasma dataengine to access SANE scanners via libksane
+ * Copyright 2012 Elias Probst <mail@eliasprobst.eu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "saneengine.h"
+
+#include <QFooTODO>
+
+#include <KBarTODO>
+
+#include <Plasma/DataContainer>
+
+SaneEngine::SaneEngine(QObject* parent, const QVariantList& args)
+    : Plasma::DataEngine(parent, args)
+{
+    // We ignore any arguments - data engines don't have any use for them
+    Q_UNUSED(args)
+
+    // This prevents applet from setting a way too high update interval and
+    // using too much system resources.
+    // This means, an update can only be done every 0.5 seconds
+    setMinimumPollingInterval(500);
+};
+
+bool SaneEngine::sourceRequestEvent(const QString &name)
+{
+    // We don't have any special code to execute the first time a source is
+    // requested, so we just call updateSourceEvent().
+    return updateSourceEvent(name);
+}
+
+QStringList SaneEngine::sources() const
+{
+    return QStringList() << "Scanners";
+}
+
+bool SaneEngine::updateSourceEvent(const QString &operation)
+{
+    QString scannerlist;
+
+    if (operation == I18N_NOOP("listScanners")) {
+        return QStringList() << "default" << "Epson LIDA 210" << "SANE dummy";
+    }
+}
---
+++ saneengine.h
@@ -0,0 +1,41 @@
+/*
+ * A KDE Plasma dataengine to access SANE scanners via libksane
+ * Copyright 2012 Elias Probst <mail@eliasprobst.eu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SANEENGINE_H
+#define SANEENGINE_H
+
+#include <Plasma/DataEngine>
+
+class SaneEngine : public Plasma::DataEngine
+{
+    // required since Plasma::DataEngine inherits QObject
+    Q_OBJECT
+
+    public:
+        // every engine needs a constructor with these arguments
+        SaneEngine(QObject* parent, const QVariantList& args);
+
+    protected:
+        // this virtual function is called when a new source is requested
+        bool sourceRequestEvent(const QString& name);
+
+        // this virtual function is called when an automatic update is
+        // triggered for an existing source (ie: when a valid update
+        // interval is set when requesting a source)
+        bool updateSourceEvent(const QString& source);
+};
